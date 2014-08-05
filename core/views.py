3dbc27af11f6970a3e8c1a7426db595c025c6521
Dimitar Ianakiev <root@mitaka-g.net>
2014-08-05 21:43:44 +0300

    * core/views.py: 	- Substituted ugettext with ugettext_lazy.

--- core/views.py
+++ core/views.py
@@ -7,7 +7,7 @@ from django.core.urlresolvers import reverse
 from django.db import IntegrityError
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy as _
 from django.contrib import messages
 from django.shortcuts import render_to_response
 from django.template import RequestContext
