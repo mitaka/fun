bb625ee77de902ff51e4d93cc88cad7774a15269
Dimitar Ianakiev <root@mitaka-g.net>
2015-02-11 21:56:35 +0200

    * Modified admin listing for authors.

--- core/admin.py
+++ core/admin.py
@@ -15,7 +15,7 @@ class PostAdmin(SummernoteModelAdmin):
 admin.site.register(Post, PostAdmin)
 
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ['email', '__str__', 'username', 'is_active']
+    list_display = ['email', '__str__', 'username', 'is_active', 'receive_update']
     filter_display = ['is_active']
 admin.site.register(Author, AuthorAdmin)
 
