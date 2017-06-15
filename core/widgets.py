0582a4df9c834754579273bd6cbbaa75c4b3aa6f
Dimitar Ianakiev <root@mitaka-g.net>
2017-06-16 00:18:57 +0300

    * core/widgets.py:         - Removed workarounds for summernote plugin to work as they are no longer needed.
    Signed-off-by: Dimitar Ianakiev <root@mitaka-g.net>

--- core/widgets.py
+++ core/widgets.py
@@ -1,5 +1,5 @@
 from django.contrib.admin.widgets import AdminFileWidget
-from django_summernote.widgets import SummernoteInplaceWidget, _static_url
+from django_summernote.widgets import SummernoteInplaceWidget
 from django.utils.safestring import mark_safe
 from django.conf import settings
 
@@ -7,18 +7,6 @@ from django.conf import settings
 class SummernoteCustomWidget(SummernoteInplaceWidget):
     class Media:
         extend = False
-        css = {'all': ('//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css',
-                       '//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css',) + (
-            _static_url('django_summernote/summernote.css'),
-        )}
-
-        js = ('//code.jquery.com/jquery-1.9.1.min.js',
-              '//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js') + (
-            _static_url('django_summernote/jquery.ui.widget.js'),
-            _static_url('django_summernote/jquery.iframe-transport.js'),
-            _static_url('django_summernote/jquery.fileupload.js'),
-            _static_url('django_summernote/summernote.min.js'),
-        )
 
 
 class AdminImageWidget(AdminFileWidget):
